a8e6809d0412bd217217c4c329a06ed53b41a76a
tonyresendiz <anthony.resendiz820@myci.csuci.edu>
2016-02-26 23:04:11 -0800

    Added files via upload

---
+++ Lab5/BankAccount.java
@@ -0,0 +1,56 @@
+package atmsimulator;
+/**
+   A bank account has a balance that can be changed by 
+   deposits and withdrawals.
+*/
+public class BankAccount
+{  
+   /**
+      Constructs a bank account with a zero balance.
+   */
+   public BankAccount()
+   {  
+      balance = 0;
+   }
+
+   /**
+      Constructs a bank account with a given balance.
+      @param initialBalance the initial balance
+   */
+   public BankAccount(double initialBalance)
+   {  
+      balance = initialBalance;
+   }
+ 
+   /** 
+      Deposits money into the account.
+      @param amount the amount of money to withdraw
+   */
+   public void deposit(double amount) 
+   {  
+      balance += amount;
+   }
+
+   /** 
+      Withdraws money from the account.
+      @param amount the amount of money to deposit
+   */
+   public void withdraw(double amount) 
+   {  
+       if (balance < amount)
+           System.out.println("Insufficient funds.");
+       else balance -= amount;
+   }
+
+   /** 
+      Gets the account balance.
+      @return the account balance
+   */
+   public double getBalance()
+   {  
+      return balance; 
+   }
+
+   private double balance; 
+}
+
